f2c4e159e964e50a7e79c0519e3dd446f6d0fe9a
Adam Maas <adamjm1220@gmail.com>
2025-04-05 20:18:18 -0400

    Updated ap-setup.service

--- export-image/06-usb-gadget/00-run.sh
+++ export-image/06-usb-gadget/00-run.sh
@@ -1,14 +1,25 @@
 #!/bin/bash -e
 
-# Enable SSH over USB (gadget mode)
-echo "dtoverlay=dwc2" >> "${ROOTFS_DIR}/boot/config.txt"
+BOOT_DIR="${ROOTFS_DIR}/boot"
+FIRMWARE_DIR="${ROOTFS_DIR}/boot/firmware"
 
-# Append modules to /etc/modules
+# Enable SSH over USB (gadget mode)
+if [ -f "${FIRMWARE_DIR}/config.txt" ]; then
+  echo "dtoverlay=dwc2" >> "${FIRMWARE_DIR}/config.txt"
+elif [ -f "${BOOT_DIR}/config.txt" ]; then
+  echo "dtoverlay=dwc2" >> "${BOOT_DIR}/config.txt"
+fi
+
+# Append modules
 echo "dwc2" >> "${ROOTFS_DIR}/etc/modules"
 echo "g_ether" >> "${ROOTFS_DIR}/etc/modules"
 
 # Enable USB ethernet gadget
-sed -i 's/\(.*rootwait\)/\1 modules-load=dwc2,g_ether/' "${ROOTFS_DIR}/boot/cmdline.txt"
+if [ -f "${FIRMWARE_DIR}/cmdline.txt" ]; then
+  sed -i 's/\(.*rootwait\)/\1 modules-load=dwc2,g_ether/' "${FIRMWARE_DIR}/cmdline.txt"
+elif [ -f "${BOOT_DIR}/cmdline.txt" ]; then
+  sed -i 's/\(.*rootwait\)/\1 modules-load=dwc2,g_ether/' "${BOOT_DIR}/cmdline.txt"
+fi
 
-# Create empty ssh file to auto-enable SSH
-touch "${ROOTFS_DIR}/boot/ssh"
+# Enable SSH
+touch "${FIRMWARE_DIR}/ssh" || touch "${BOOT_DIR}/ssh"
