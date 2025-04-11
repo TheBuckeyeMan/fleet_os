1cd68f5f5779e410feb5887593a19a5637e2d644
Adam Maas <adamjm1220@gmail.com>
2025-04-11 08:52:22 -0400

    Added dnsmasq.service

--- stage3/03-dnsmasq/00-run.sh
+++ stage3/03-dnsmasq/00-run.sh
@@ -2,18 +2,19 @@
 
 echo "[Stage 03-dnsmasq - START] $(date)" >> "${ROOTFS_DIR}/boot/firmware/build-stage-logs.txt"
 
-# Update and Install dnsmasq
+# Install dnsmasq-base
 on_chroot << EOF
 apt-get update
 apt-get install -y dnsmasq-base
 EOF
 
-# Copy over dnsmasq config
+# Copy config + custom service
 install -m 644 files/dnsmasq.conf "${ROOTFS_DIR}/etc/dnsmasq.conf"
+install -m 644 files/dnsmasq.service "${ROOTFS_DIR}/etc/systemd/system/dnsmasq.service"
 
-# Enable dnsmasq service
 on_chroot << EOF
-systemctl enable dnsmasq
+systemctl enable dnsmasq.service
 EOF
 
 echo "[Stage 03-dnsmasq - END] $(date)" >> "${ROOTFS_DIR}/boot/firmware/build-stage-logs.txt"
+
