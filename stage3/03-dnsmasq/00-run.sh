5a1a433997e910f3baabd625ccc144da8041edf1
Adam Maas <adamjm1220@gmail.com>
2025-04-10 21:20:00 -0400

    Complete Statge Refactor

--- stage3/03-dnsmasq/00-run.sh
+++ stage3/03-dnsmasq/00-run.sh
@@ -1,14 +1,19 @@
 #!/bin/bash -e
 
-echo "[Stage X - START] $(date)" >> "${ROOTFS_DIR}/boot/firmware/build-stage-logs.txt"
+echo "[Stage 03-dnsmasq - START] $(date)" >> "${ROOTFS_DIR}/boot/firmware/build-stage-logs.txt"
 
-
+# Update and Install dnsmasq
+on_chroot << EOF
+apt-get update
 apt-get install -y dnsmasq
+EOF
 
+# Copy over dnsmasq config
 install -m 644 files/dnsmasq.conf "${ROOTFS_DIR}/etc/dnsmasq.conf"
 
+# Enable dnsmasq service
 on_chroot << EOF
 systemctl enable dnsmasq
 EOF
 
-echo "[Stage X - END] $(date)" >> "${ROOTFS_DIR}/boot/firmware/build-stage-logs.txt"
+echo "[Stage 03-dnsmasq - END] $(date)" >> "${ROOTFS_DIR}/boot/firmware/build-stage-logs.txt"
