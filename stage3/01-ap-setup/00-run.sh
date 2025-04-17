0a0712a54309722c8d8646a2c51d7e44413deca1
Adam Maas <adamjm1220@gmail.com>
2025-04-17 10:55:47 -0400

    added ap-mode.nmconnection

--- stage3/01-ap-setup/00-run.sh
+++ stage3/01-ap-setup/00-run.sh
@@ -1,21 +1,32 @@
 #!/bin/bash -e
 
+# Log build start
 mkdir -p "${ROOTFS_DIR}/boot/firmware"
 echo "[Stage 01-ap-setup - START] $(date)" >> "${ROOTFS_DIR}/boot/firmware/build-stage-logs.txt"
 
-
+# Install Flask
 on_chroot << EOF
 apt-get update
 apt-get install -y python3-flask
 EOF
 
-# Copy python server and service
-install -m 755 files/setup-server.py "$ROOTFS_DIR/usr/local/bin/setup-server.py"
-install -m 644 files/ap-setup.service "$ROOTFS_DIR/etc/systemd/system/ap-setup.service"
+# Copy Flask server and systemd service
+install -m 755 files/setup-server.py "${ROOTFS_DIR}/usr/local/bin/"
+install -m 644 files/ap-setup.service "${ROOTFS_DIR}/etc/systemd/system/"
 
-# Enable the service
+# Enable the Flask systemd service
 on_chroot << EOF
 systemctl enable ap-setup.service
 EOF
 
+# Copy static NetworkManager AP profile
+install -D -m 600 files/ap-mode.nmconnection "${ROOTFS_DIR}/etc/NetworkManager/system-connections/ap-mode.nmconnection"
+
+# Ensure proper permissions and reload connection
+on_chroot << EOF
+chmod 600 /etc/NetworkManager/system-connections/ap-mode.nmconnection
+nmcli connection reload
+EOF
+
+# Log build end
 echo "[Stage 01-ap-setup - END] $(date)" >> "${ROOTFS_DIR}/boot/firmware/build-stage-logs.txt"
