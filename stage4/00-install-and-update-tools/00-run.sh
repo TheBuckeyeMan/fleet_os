f14f12aaff4d6ed2e24740523569a78b81b1f4b6
Adam Maas <adamjm1220@gmail.com>
2025-05-11 11:36:43 -0400

    Added stage4 to config

--- stage4/00-install-and-update-tools/00-run.sh
+++ stage4/00-install-and-update-tools/00-run.sh
@@ -1,7 +1,5 @@
 #!/bin/bash -e
 
-echo "[Stage 04 - Install/Update Tools START] $(date)" >> "${ROOTFS_DIR}/boot/firmware/build-stage-logs.txt"
-
 # Install the script and systemd unit
 install -m 755 files/post-wifi-update.sh "${ROOTFS_DIR}/usr/local/bin/post-wifi-update.sh"
 install -m 644 files/post-wifi-update.service "${ROOTFS_DIR}/etc/systemd/system/post-wifi-update.service"
@@ -10,5 +8,3 @@ install -m 644 files/post-wifi-update.service "${ROOTFS_DIR}/etc/systemd/system/
 on_chroot << EOF
 systemctl enable post-wifi-update.service
 EOF
-
-echo "[Stage 04 - Install/Update Tools END] $(date)" >> "${ROOTFS_DIR}/boot/firmware/build-stage-logs.txt"
